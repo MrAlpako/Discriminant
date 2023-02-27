fb519dcaff7fd9cb0d19ead52f6115ed65f00eb1
MrAlpako <124809137+MrAlpako@users.noreply.github.com>
2023-02-27 21:24:34 +0300

    create Discriminant.cpp

---
+++ Discriminant.cpp
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	float a,b,c;
+	printf("a=");
+	scanf("%f",&a);
+	printf("b=");
+	scanf("%f",&b);
+	printf("c=");
+	scanf("%f",&c);
+	
+	if(a!=0)
+	{
+	
+	float d=b*b-4*a*c;
+	
+	if(d>0)
+	{
+		float x1=(-b-sqrt(d))/(2*a);
+		float x2=(-b+sqrt(d))/(2*a);
+		printf("x1=%f x2=%f",x1,x2);
+	}
+	else
+	{
+		if(d==0)
+		{
+			float x=(-b)/(2*a);
+			printf("x=%f",x);
+		}
+		else
+		{
+			printf("D<0");
+		}
+	}
+}
+	else
+	{
+		float x=-c/b;
+		printf("x=%f",x);
+	}
+}
